406a45cb94f0ce82673226883a474c2f8aed4484
Frederic Lauron <flauron-23@node19.enst.fr>
2025-11-26 17:41:33 +0100

    add experiment refactor

---
+++ src/experiment/context.py
@@ -0,0 +1,201 @@
+import os
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from custom_comp.datasets import ImageFolder
+from custom_comp.zoo import models
+
+from utils import CustomDataParallel,configure_optimizers
+from utils.loss import RateDistortionLoss
+from utils.dataset import TestKodakDataset
+from utils.chengBA2 import inject_adapter,freeze_model_with_switch
+from utils.masks import generate_mask_from_structured,lambda_percentage,\
+                        generate_mask_from_unstructured,\
+                        generate_mask_from_structured_fisher
+
+class Context:
+    """Experiment context"""
+
+    def __init__(self, args,project_run_path):
+        self.args = args
+        self.project_run_path = project_run_path
+
+        self.log_wandb = True
+
+        #File paths for saving images and data
+        self.img_dir = self._get_folder_path("imgs")
+        self._make_dir(self.img_dir)
+
+        self.data_dir = self._get_folder_path("data")
+        self._make_dir(self.data_dir)
+
+        self.mask_dir = self._get_sub_folder_path(self.data_dir,'masks')
+        self._make_dir(self.mask_dir)
+
+        self.res_dir = self._get_sub_folder_path(self.data_dir,'results')
+        self._make_dir(self.res_dir)
+
+        self.model_dir = self._get_sub_folder_path(self.data_dir,'models')
+        self._make_dir(self.model_dir)
+
+        # configure device, dataloaders, model, 
+        self.device = self._get_device(args)
+        self.train_dataloader, self.val_dataloader, self.kodak_dataloader = self._get_dataloaders(args)
+        self.net = self._get_model(args)
+
+        # Adapter
+        if args.pruningType=="adapter":
+            inject_adapter(self.net,args.rank,args.alpha) 
+
+        #Put model on GPU after injecting adapter
+        self.net=self.net.to(self.device)
+
+        # configure optimizers, criterion, lr scheduler
+        self.optimizer, self.aux_optimizer = self._get_optimizers(self.net, args)
+        self.criterion = self._get_criterion(args)
+        self.lr_scheduler = self._get_lr_scheduler(self.optimizer, args)
+        
+        # Adapter
+        # Done after optimizer to avoid trigger error
+        # Froze all the parameters except the switches
+        if args.pruningType =="adapter":
+            freeze_model_with_switch(self.net)
+
+        self.last_epoch = 0
+        self.best_val_loss = float("inf")
+        self.best_kodak_loss = float("inf")
+        
+        # checkpoint
+        if args.checkpoint is not None:
+            self._get_checkpoint(args)
+
+        # Need to be set even for adapter to avoid errors
+        self.alpha = np.linspace(0.0, args.maxPrunning, args.maxPoint)[::-1]
+        self.lambda_list , self.amounts = lambda_percentage(self.alpha, amount = args.maxPrunning)
+        
+        # Masks
+        if args.mask:
+            self.all_mask, self.parameters_to_prune = self._get_mask(args)
+    
+    def _get_folder_path(self,folder_name):
+        return os.path.join(self.project_run_path, "..", folder_name, self.args.nameRun)       
+
+    def _get_sub_folder_path(self,folder,subfolder):
+        return os.path.join(folder,subfolder)
+       
+    def _make_dir(self,dir,exist_ok=True):
+        os.makedirs(dir, exist_ok=exist_ok)
+
+    def _get_device(self,args):
+        return "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
+    
+    # def _multi_gpu(self,net,args):
+    #     if args.cuda and torch.cuda.device_count() > 1:
+    #         print(f'Training on: {torch.cuda.device_count()} GPUs')
+    #         net = CustomDataParallel(net)
+    #     return net
+    
+    def _get_dataloaders(self,args):
+
+        train_transforms = transforms.Compose([transforms.RandomCrop(args.patch_size), transforms.ToTensor()])
+        test_transforms = transforms.Compose([transforms.CenterCrop(args.patch_size), transforms.ToTensor()])
+
+        train_dataset = ImageFolder(args.dataset, split="train", transform=train_transforms)
+        test_dataset = ImageFolder(args.dataset, split="test", transform=test_transforms)
+        kodak_dataset = TestKodakDataset(data_dir = args.test_dir) # Kodak test set
+
+        train_dataloader = DataLoader(
+            train_dataset,
+            batch_size=args.batch_size,
+            num_workers=args.num_workers,
+            shuffle=True,
+            pin_memory=(self.device == "cuda"),
+            )
+
+        val_dataloader = DataLoader(
+            test_dataset,
+            batch_size=args.test_batch_size,
+            num_workers=args.num_workers,
+            shuffle=False,
+            pin_memory=(self.device == "cuda"),
+            )
+
+        kodak_dataloader = DataLoader(
+            kodak_dataset, 
+            shuffle=False, 
+            batch_size=1, 
+            pin_memory=(self.device == "cuda"), 
+            num_workers= args.num_workers 
+            )
+        
+        return train_dataloader, val_dataloader, kodak_dataloader
+    
+    def _get_model(self,args):
+        net = models[args.model]()
+        #net = net.to(self.device)
+
+        # multi GPUs
+        if args.cuda and torch.cuda.device_count() > 1:
+            print(f'Training on: {torch.cuda.device_count()} GPUs')
+            net = CustomDataParallel(net)
+        else:
+            print('Training on a single GPU or CPU')
+
+        return net
+
+    def _get_optimizers(self,net,args):
+        optimizer, aux_optimizer = configure_optimizers(net, args)
+        return optimizer, aux_optimizer
+    
+    def _get_criterion(self,args):
+        return RateDistortionLoss(lmbda=args.lmbda)
+    
+    def _get_lr_scheduler(self,optimizer,args):
+        return optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min", factor=0.3, patience=4)
+ 
+    def _get_mask(self,args):
+
+        all_mask,parameters_to_prune = {}, {}
+
+        if args.pruningType =="structured":
+            if args.fisher:
+                all_mask["g_a"], parameters_to_prune["g_a"] = generate_mask_from_structured_fisher(self.net,self.amounts,self.train_dataloader,self.criterion,self.net.g_a)
+                all_mask["g_s"], parameters_to_prune["g_s"] = generate_mask_from_structured_fisher(self.net,self.amounts,self.train_dataloader,self.criterion,self.net.g_s)
+            else:
+                all_mask["g_a"], parameters_to_prune["g_a"] = generate_mask_from_structured(self.net.g_a, self.amounts)
+                all_mask["g_s"], parameters_to_prune["g_s"] = generate_mask_from_structured(self.net.g_s, self.amounts)
+
+        elif args.pruningType == "unstructured":
+                all_mask["g_a"], parameters_to_prune["g_a"] = generate_mask_from_unstructured(self.net.g_a, self.amounts)
+                all_mask["g_s"], parameters_to_prune["g_s"] = generate_mask_from_unstructured(self.net.g_s, self.amounts)
+
+        # Save masks 
+        torch.save(all_mask, f"{self.mask_dir}/mask_{args.nameRun}.pth")
+        torch.save(parameters_to_prune, f"{self.mask_dir}/parameters_to_prune_{args.nameRun}.pth")
+        
+        return all_mask, parameters_to_prune
+    
+    def _get_checkpoint(self,args):
+
+        print("Loading", args.checkpoint)
+        checkpoint = torch.load(args.checkpoint, map_location=self.device)
+        self.net.load_state_dict(checkpoint["state_dict"])
+
+        if args.resume_train:
+            print('Loading elements from checkpoint to resume the training')
+            self.last_epoch = checkpoint["epoch"] + 1
+
+            self.optimizer.load_state_dict(checkpoint["optimizer"])
+            if self.aux_optimizer is not None and checkpoint["aux_optimizer"] is not None:
+                self.aux_optimizer.load_state_dict(checkpoint["aux_optimizer"])
+            self.lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
+
+            self.best_val_loss = checkpoint["best_val_loss"]
+            self.best_kodak_loss = checkpoint["best_kodak_loss"] 
---
+++ src/experiment/experiment.py
@@ -0,0 +1,318 @@
+import torch.optim as optim
+
+from utils import save_checkpoint
+from utils.engine import test_epoch,train_one_epoch, compress_one_epoch #, AverageMeter,pad,crop
+from utils.masks import delete_mask, apply_saved_mask
+
+from utils.chengBA2 import set_index_switch,measure_switch_sparcity,measure_sparsity_induce_by_switch
+
+from evaluate import plot_rate_distorsion
+
+from compressai.zoo import cheng2020_attn,mbt2018_mean
+
+from experiment.context import Context
+
+import os
+import json
+import wandb
+
+import wandb, matplotlib.pyplot as plt,inspect
+
+class Experiment:
+    def __init__(self,args,project_run_path):
+        super().__init__()
+        self.args = args
+        self.ctx  = Context(args,project_run_path)
+    
+    def make_baseline_plot(self):
+            bpp_list = []
+            psnr_list = []
+            mssim_list = []
+
+            ref_bpp_list = []
+            ref_psnr_list = []
+            ref_mssim_list = []         
+
+            print("Make actual compression")
+            self.ctx.net.update(force = True)
+
+            for index in range(len(self.ctx.lambda_list)):
+
+                if index != len(self.ctx.lambda_list)-1:
+                    
+                    apply_saved_mask(self.ctx.net.g_a, self.ctx.all_mask["g_a"][index])
+                    apply_saved_mask(self.ctx.net.g_s, self.ctx.all_mask["g_s"][index])
+
+                    bpp_ac, psnr_ac, mssim_ac = compress_one_epoch(self.ctx.net, self.ctx.kodak_dataloader, self.ctx.device)
+
+                    delete_mask(self.ctx.net.g_a,self.ctx.parameters_to_prune["g_a"])
+                    delete_mask(self.ctx.net.g_s,self.ctx.parameters_to_prune["g_s"])
+
+                # No mask for 0.483 lambda 0.0 amount pruning   
+                else:
+                    bpp_ac, psnr_ac, mssim_ac = compress_one_epoch(self.ctx.net, self.ctx.kodak_dataloader, self.ctx.device)
+                
+                bpp_list.append(bpp_ac)
+                psnr_list.append(psnr_ac)
+                mssim_list.append(mssim_ac)
+
+            for index in range(6):
+                refnet = cheng2020_attn(quality=index+1,pretrained=True).to(self.ctx.device)
+                ref_bpp_ac, ref_psnr_ac, ref_mssim_ac = compress_one_epoch(refnet, self.ctx.kodak_dataloader, self.ctx.device)
+
+                ref_bpp_list.append(ref_bpp_ac)
+                ref_psnr_list.append(ref_psnr_ac)
+                ref_mssim_list.append(ref_mssim_ac)
+
+            psnr_res = {}
+            mssim_res = {}
+            bpp_res = {} 
+
+            bpp_res["ours"] = bpp_list
+            psnr_res["ours"] = psnr_list
+            mssim_res["ours"] = mssim_list
+            
+            bpp_res[self.args.model] = ref_bpp_list
+            psnr_res[self.args.model] = ref_psnr_list
+            mssim_res[self.args.model] = ref_mssim_list
+
+            plot_rate_distorsion(bpp_res, psnr_res, 
+                                0, eest="compression_before_training", 
+                                metric = 'PSNR',
+                                save_fig=True,
+                                file_name=os.path.join(self.ctx.img_dir, f"{self.args.nameRun}_psnr_minus_1.png"), #TODO change naming to avoid duplicate code
+                                log_wandb=self.ctx.log_wandb)
+
+            plot_rate_distorsion(bpp_res, 
+                                mssim_res, 
+                                0, 
+                                eest="compression_mssim_before_training", 
+                                metric = 'MS-SSIM',
+                                save_fig=True,
+                                file_name=os.path.join(self.ctx.img_dir, f"{self.args.nameRun}_mssim_minus_1.png"),
+                                log_wandb=self.ctx.log_wandb, is_psnr=False)
+            
+
+            #Save data dictionnary
+            results = {"psnr": psnr_res,"mssim": mssim_res,"bpp": bpp_res}
+            file_path = os.path.join(self.ctx.res_dir, f"data_{self.args.nameRun}_minus_1.json")
+            with open(file_path, "w") as f:
+                json.dump(results, f)
+
+    def make_plot(self,epoch):
+            bpp_list = []
+            psnr_list = []
+            mssim_list = []
+
+            ref_bpp_list = []
+            ref_psnr_list = []
+            ref_mssim_list = []         
+
+            print("Make actual compression")
+            self.ctx.net.update(force = True)
+
+            if self.args.mask:
+                for index in range(len(self.ctx.lambda_list)):
+
+                    if index != len(self.ctx.lambda_list)-1:
+                        
+                        apply_saved_mask(self.ctx.net.g_a, self.ctx.all_mask["g_a"][index])
+                        apply_saved_mask(self.ctx.net.g_s, self.ctx.all_mask["g_s"][index])
+
+                        bpp_ac, psnr_ac, mssim_ac = compress_one_epoch(self.ctx.net, self.ctx.kodak_dataloader, self.ctx.device)
+
+                        delete_mask(self.ctx.net.g_a,self.ctx.parameters_to_prune["g_a"])
+                        delete_mask(self.ctx.net.g_s,self.ctx.parameters_to_prune["g_s"])
+
+                    # No mask for 0.483 lambda 0.0 amount pruning   
+                    else:
+                        bpp_ac, psnr_ac, mssim_ac = compress_one_epoch(self.ctx.net, self.ctx.kodak_dataloader, self.ctx.device)
+                    
+                    bpp_list.append(bpp_ac)
+                    psnr_list.append(psnr_ac)
+                    mssim_list.append(mssim_ac)
+
+                for index in range(6):
+                    refnet = self._get_ref_model(quality=index+1).to(self.ctx.device)
+                    ref_bpp_ac, ref_psnr_ac, ref_mssim_ac = compress_one_epoch(refnet, self.ctx.kodak_dataloader, self.ctx.device)
+
+                    ref_bpp_list.append(ref_bpp_ac)
+                    ref_psnr_list.append(ref_psnr_ac)
+                    ref_mssim_list.append(ref_mssim_ac)
+            
+            elif self.args.pruningType=="adapter":
+                for index in range(6): #0 = max pruning, 5 = no pruning
+
+                    # Update index 
+                    # net.set_index(index)
+                    set_index_switch(self.ctx.net,index)
+
+                    bpp_ac, psnr_ac, mssim_ac = compress_one_epoch(self.ctx.net, self.ctx.kodak_dataloader, self.ctx.device)
+
+                    bpp_list.append(bpp_ac)
+                    psnr_list.append(psnr_ac)
+                    mssim_list.append(mssim_ac)
+
+                    #Compute reference from cheng202_attn
+                    refnet = self._get_ref_model(quality=index+1).to(self.ctx.device)
+                    ref_bpp_ac, ref_psnr_ac, ref_mssim_ac = compress_one_epoch(refnet, self.ctx.kodak_dataloader, self.ctx.device)
+
+                    ref_bpp_list.append(ref_bpp_ac)
+                    ref_psnr_list.append(ref_psnr_ac)
+                    ref_mssim_list.append(ref_mssim_ac)
+
+            psnr_res = {}
+            mssim_res = {}
+            bpp_res = {} 
+
+            bpp_res["ours"] = bpp_list
+            psnr_res["ours"] = psnr_list
+            mssim_res["ours"] = mssim_list
+            
+            bpp_res[self.args.model] = ref_bpp_list
+            psnr_res[self.args.model] = ref_psnr_list
+            mssim_res[self.args.model] = ref_mssim_list
+
+            plot_rate_distorsion(bpp_res, psnr_res, 
+                                epoch, eest="compression", 
+                                metric = 'PSNR',
+                                save_fig=True,
+                                file_name=os.path.join(self.ctx.img_dir, f"{self.args.nameRun}_psnr_{epoch}.png"), #TODO change naming to avoid duplicate code
+                                log_wandb=self.ctx.log_wandb)
+
+            plot_rate_distorsion(bpp_res, 
+                                mssim_res, 
+                                epoch, 
+                                eest="compression_mssim", 
+                                metric = 'MS-SSIM',
+                                save_fig=True,
+                                file_name=os.path.join(self.ctx.img_dir, f"{self.args.nameRun}_mssim_{epoch}.png"),
+                                log_wandb=self.ctx.log_wandb, is_psnr=False)
+            
+
+            if self.args.pruningType=="adapter":
+                measure_switch_sparcity(self.ctx.net,epoch)
+                measure_sparsity_induce_by_switch(self.ctx.net,epoch)
+
+
+            #Save data dictionnary
+            results = {"psnr": psnr_res,"mssim": mssim_res,"bpp": bpp_res}
+            file_path = os.path.join(self.ctx.res_dir, f"data_{self.args.nameRun}_{epoch}.json")
+            with open(file_path, "w") as f:
+                json.dump(results, f)
+
+    def train(self,epoch):
+        print(f"Learning rate: {self.ctx.optimizer.param_groups[0]['lr']}")
+
+        # Training 
+        loss_tot_train, bpp_train, mse_train, aux_train_loss = train_one_epoch(
+            self.ctx.net,
+            self.ctx.criterion,
+            self.ctx.train_dataloader,
+            self.ctx.optimizer,
+            self.ctx.aux_optimizer,
+            epoch,
+            self.args.clip_max_norm,
+            args_mask=self.args.mask,
+            all_mask=self.ctx.all_mask if self.args.mask  else None,
+            lambda_list=self.ctx.lambda_list if self.args.mask else None,
+            parameters_to_prune=self.ctx.parameters_to_prune if self.args.mask  else None,
+            probs=None
+        )
+
+        if self.ctx.log_wandb:
+            wandb.log({
+                "train/epoch":epoch,
+                "train/loss": loss_tot_train,
+                "train/bpp_loss": bpp_train,
+                "train/mse_loss": mse_train,
+                "train/aux_loss":aux_train_loss
+            },step = epoch)      
+
+    def validate(self,epoch, dataloader,tag):
+        total_losses=[]
+        is_best_kodak = False
+        is_best_val = False
+        for i in range(len(self.ctx.lambda_list)):
+
+            if self.args.mask :
+                if i != len(self.ctx.lambda_list)-1:
+
+                    apply_saved_mask(self.ctx.net.g_a,self.ctx.all_mask["g_a"][i])
+                    apply_saved_mask(self.ctx.net.g_s,self.ctx.all_mask["g_s"][i])
+
+                    loss_tot_val, bpp_loss_val, mse_loss_val, aux_loss_val, psnr_val, ssim_val = test_epoch(epoch, dataloader, self.ctx.net, self.ctx.criterion, tag)
+                    
+                    delete_mask(self.ctx.net.g_a,self.ctx.parameters_to_prune["g_a"])
+                    delete_mask(self.ctx.net.g_s,self.ctx.parameters_to_prune["g_s"])
+                else:
+                    loss_tot_val, bpp_loss_val, mse_loss_val, aux_loss_val, psnr_val, ssim_val = test_epoch(epoch,dataloader, self.ctx.net, self.ctx.criterion, tag)
+
+            # Adapter pruning type
+            else:
+                set_index_switch(self.ctx.net,i)
+
+                loss_tot_val, bpp_loss_val, mse_loss_val, aux_loss_val, psnr_val, ssim_val = test_epoch(epoch, dataloader, self.ctx.net, self.ctx.criterion, tag)
+
+            if self.ctx.log_wandb:
+                wandb.log({
+                    f"{tag}/epoch":epoch,
+                    f"{tag}_{i}/loss": loss_tot_val,
+                    f"{tag}_{i}/bpp_loss": bpp_loss_val,
+                    f"{tag}_{i}/mse_loss": mse_loss_val,
+                    f"{tag}_{i}/aux_loss":aux_loss_val,
+                    f"{tag}_{i}/psnr":psnr_val,
+                    f"{tag}_{i}/mssim":ssim_val,
+                },step = epoch)   
+
+            total_losses.append(loss_tot_val)
+        
+
+        avg_loss_tot_val = sum(total_losses) / len(total_losses)
+
+        if tag == 'val':
+            avg_loss_tot_val = sum(total_losses) / len(total_losses)
+            if isinstance(self.ctx.lr_scheduler, optim.lr_scheduler.ReduceLROnPlateau):
+                self.ctx.lr_scheduler.step(avg_loss_tot_val)
+            else:
+                self.ctx.lr_scheduler.step()
+            
+            # save checkpoint according to val_loss
+            is_best_val = avg_loss_tot_val < self.ctx.best_val_loss
+            self.ctx.best_val_loss = min(avg_loss_tot_val, self.ctx.best_val_loss)
+        
+        elif tag == 'kodak':
+            # avg_loss_tot_kodak = sum(total_losses) / len(total_losses)
+            is_best_kodak = avg_loss_tot_val < self.ctx.best_kodak_loss
+            self.ctx.best_kodak_loss = min(avg_loss_tot_val, self.ctx.best_kodak_loss)
+        else:
+            # do nothing
+            pass 
+
+        if self.args.save and (is_best_kodak or is_best_val):
+            save_checkpoint(
+                {
+                    "epoch": epoch,
+                    "state_dict": self.ctx.net.state_dict(),
+                    "best_val_loss": self.ctx.best_val_loss,
+                    "best_kodak_loss":self.ctx.best_kodak_loss,
+                    "optimizer": self.ctx.optimizer.state_dict(),
+                    "aux_optimizer": self.ctx.aux_optimizer.state_dict() if self.ctx.aux_optimizer is not None else None,
+                    "lr_scheduler": self.ctx.lr_scheduler.state_dict(),
+                },
+                True, # is best
+                out_dir=self.ctx.model_dir,
+                #filename=f"{str(args.lmbda).replace('0.','')}_checkpoint.pth.tar"
+                filename=f"{self.args.nameRun}_checkpoint.pth.tar"
+            )
+        
+
+    def _get_ref_model(self,quality):
+        """
+        get reference model with the provided quality
+        """
+        if self.args.model == "cheng":
+            return cheng2020_attn(quality,pretrained = True)
+        elif self.args.model == "msh":
+            return mbt2018_mean(quality,pretrained = True)
+        # elif self.args.model == "cheng":
